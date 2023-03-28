d2416fff8299e6665a31f7c2daea760675e68800
Kerem Mican <47419589+keremmican@users.noreply.github.com>
2023-03-29 00:12:07 +0300

    register, eslint, yarn and some packages

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async';
-
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 import { styled } from '@mui/material/styles';
+import * as React from 'react';
 import {
     Link,
     Container,
@@ -10,12 +12,13 @@ import {
     Button,
     TextField,
     InputAdornment,
-    IconButton, Checkbox, Select, MenuItem, Box
+    IconButton, Checkbox, Select, MenuItem, Box, Snackbar
 } from '@mui/material';
 import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {DatePicker, LoadingButton, LocalizationProvider} from "@mui/lab";
+import {Alert, DatePicker, LoadingButton, LocalizationProvider} from "@mui/lab";
 import AdapterDayjs from "@mui/lab/AdapterDayjs";
+import axios from "axios";
 import Iconify from "../components/iconify";
 
 const StyledRoot = styled('div')(({ theme }) => ({
@@ -37,16 +40,20 @@ const BASE_URL = process.env.REACT_APP_URL
 
 export default function RegisterPage() {
     const navigate = useNavigate();
-
+    const [toastOpen, setToastOpen] = useState(false)
     const [showPassword, setShowPassword] = useState(false);
+    const [success, setSuccess] = useState(false)
 
     const [steamId, setSteamId] = useState();
 
-    const [userName, setUserName] = useState();
-    const [email, setEmail] = useState();
-    const [pwd, setPwd] = useState();
-    const [birthDate, setBirthDate] = useState();
-    const [gender, setGender] = useState();
+    const [userName, setUserName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [pwd, setPwd] = useState(null);
+    const [birthDate, setBirthDate] = useState(null);
+    const [gender, setGender] = useState(null);
+    const [phoneNumber, setPhoneNumber] = useState(0);
+    const [firstName, setFirstName] = useState(null);
+    const [lastName, setLastName] = useState(null);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
@@ -56,15 +63,56 @@ export default function RegisterPage() {
         }
     }, []);
 
-    const handleClick = () => {
-        navigate('/home', { replace: true });
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        try {
+            const signuprequest = JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                username: userName,
+                email: email,
+                password: pwd,
+                phoneNumber: phoneNumber,
+                birthDate: birthDate
+            });
+
+            const response = axios.post(`${BASE_URL}auth/register`, signuprequest, {
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            let data;
+            await response.then((result) => {
+                data = result.data;
+                return data;
+            })
+            setSuccess(true);
+            setToastOpen(true)
+            navigate('/home', { replace: true });
+        } catch(err) {
+            console.log("hTA")
+        }
     }
+
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setToastOpen(false);
+    };
 
     return(
         <>
         <Helmet>
             <title> Register | Sutoga </title>
         </Helmet>
+
+            <Snackbar open={toastOpen} autoHideDuration={3000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    This is a success message!
+                </Alert>
+            </Snackbar>
 
         <StyledRoot>
             <Container maxWidth="sm">
@@ -74,14 +122,22 @@ export default function RegisterPage() {
                     </Typography>
 
                 <Stack spacing={3}>
-                    <TextField name="username" label="Username" />
+                    <Stack direction="row">
+                        <TextField  name="firstName" label="First Name" onChange={(e) => setFirstName(e.target.value)}/>
 
-                    <TextField name="email" label="Email address" />
+                        <Box sx={{ flexGrow: 1 }} />
+
+                        <TextField  name="lastName" label="Last Name" onChange={(e) => setLastName(e.target.value)}/>
+                    </Stack>
+                    <TextField name="username" label="Username" onChange={(e) => setUserName(e.target.value)}/>
+
+                    <TextField name="email" label="Email address" onChange={(e) => setEmail(e.target.value)}/>
 
                     <TextField
                         name="password"
                         label="Password"
                         type={showPassword ? 'text' : 'password'}
+                        onChange={(e) => setPwd(e.target.value)}
                         InputProps={{
                             endAdornment: (
                                 <InputAdornment position="end">
@@ -93,7 +149,6 @@ export default function RegisterPage() {
                         }}
                     />
 
-
                     <Stack direction="row">
                         <TextField
                             id="date"
@@ -101,6 +156,7 @@ export default function RegisterPage() {
                             type="date"
                             defaultValue="2000-01-01"
                             sx={{ width: 250 }}
+                            onChange={(e) => setBirthDate(e.target.value)}
                             InputLabelProps={{
                                 shrink: true,
                             }}
@@ -108,18 +164,12 @@ export default function RegisterPage() {
 
                         <Box sx={{ flexGrow: 1 }} />
 
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={""}
-                            label="Gender"
-                            onChange={(e) => setGender(e.target.value)}
-                            sx={{ width: 250}}
-                        >
-                            <MenuItem value={"Female"}>Female</MenuItem>
-                            <MenuItem value={"Male"}>Male</MenuItem>
-                            <MenuItem value={"Other"}>Other</MenuItem>
-                        </Select>
+                        <PhoneInput
+                            country={"tr"}
+                            inputStyle={{ borderRadius: "13px", height: "40px", width: "100%" }}
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e)}
+                        />
                     </Stack>
 
                 </Stack>
