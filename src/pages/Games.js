c59e2273b35dd534b9d536edd880cfd283aae64d
Kerem Mican <47419589+keremmican@users.noreply.github.com>
2023-04-19 11:16:52 +0300

    [Kerem Mican] Games and messages pages (in progress)

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -3,34 +3,25 @@ import React, {useState, useEffect} from "react";
 // @mui
 import {alpha, useTheme} from '@mui/material/styles';
 import {Alert} from "@mui/lab";
-import {Box, Button, Card, Container, MobileStepper, Paper, Typography} from "@mui/material";
+import {Box, Button, Card, Container, Grid, MobileStepper, Paper, Typography} from "@mui/material";
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import GameCard from "../components/cards/GameCard";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
     {
-        label: 'San Francisco – Oakland Bay Bridge, United States',
+        label: 'Eador: Genesis',
         imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+            "https://cdn.akamai.steamstatic.com/steam/apps/235660/header.jpg?t=1563274911",
     },
     {
-        label: 'Bird',
+        label: 'Eador: Genesis',
         imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bali, Indonesia',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-    },
-    {
-        label: 'Goč, Serbia',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+            "https://cdn.akamai.steamstatic.com/steam/apps/236660/header.jpg?t=1447357742",
     },
 ];
 
@@ -52,74 +43,127 @@ export default function Games() {
     };
 
     return (
-        <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-            <Paper
-                square
-                elevation={0}
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    height: 50,
-                    pl: 2,
-                    bgcolor: 'background.default',
-                }}
-            >
-                <Typography>{images[activeStep].label}</Typography>
-            </Paper>
-            <AutoPlaySwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents
-            >
-                {images.map((step, index) => (
-                    <div key={step.label}>
-                        {Math.abs(activeStep - index) <= 2 ? (
-                            <Box
-                                component="img"
+        <>
+            <Grid container justifyContent={"center"} sx={{pb: 3}}>
+                <Grid item>
+                    <Card sx={{bgcolor: "background.default"}}>
+                        <Box sx={{pl: 2, flexGrow: 1, width: 200}}>
+                            <Typography>Connect your steam account / steam account connected </Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+
+                <Grid item>
+                    <Card>
+                        <Box sx={{ maxWidth: 600, flexGrow: 1 }}>
+                            <Paper
+                                square
+                                elevation={0}
                                 sx={{
-                                    height: 255,
-                                    display: 'block',
-                                    maxWidth: 400,
-                                    overflow: 'hidden',
-                                    width: '100%',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    height: 50,
+                                    pl: 2,
+                                    bgcolor: 'background.default',
                                 }}
-                                src={step.imgPath}
-                                alt={step.label}
+                            >
+                                <Typography>{images[activeStep].label}</Typography>
+                            </Paper>
+                            <AutoPlaySwipeableViews
+                                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                                index={activeStep}
+                                onChangeIndex={handleStepChange}
+                                enableMouseEvents
+                            >
+                                {images.map((step, index) => (
+                                    <div key={step.label}>
+                                        {Math.abs(activeStep - index) <= 2 ? (
+                                            <Box
+                                                component="img"
+                                                sx={{
+                                                    display: 'block',
+                                                    overflow: 'hidden',
+                                                    width: '100%',
+                                                }}
+                                                src={step.imgPath}
+                                                alt={step.label}
+                                            />
+                                        ) : null}
+                                    </div>
+                                ))}
+                            </AutoPlaySwipeableViews>
+
+                            <MobileStepper
+                                variant="progress"
+                                steps={maxSteps}
+                                position="static"
+                                activeStep={activeStep}
+                                sx={{ maxWidth: 600, flexGrow: 1 }}
+                                nextButton={
+                                    <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                                        {theme.direction === 'rtl' ? (
+                                            <KeyboardArrowLeft />
+                                        ) : (
+                                            <KeyboardArrowRight />
+                                        )}
+                                    </Button>
+                                }
+                                backButton={
+                                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                                        {theme.direction === 'rtl' ? (
+                                            <KeyboardArrowRight />
+                                        ) : (
+                                            <KeyboardArrowLeft />
+                                        )}
+                                    </Button>
+                                }
                             />
-                        ) : null}
-                    </div>
-                ))}
-            </AutoPlaySwipeableViews>
-            <MobileStepper
-                steps={maxSteps}
-                position="static"
-                activeStep={activeStep}
-                nextButton={
-                    <Button
-                        size="small"
-                        onClick={handleNext}
-                        disabled={activeStep === maxSteps - 1}
-                    >
-                        Next
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowLeft />
-                        ) : (
-                            <KeyboardArrowRight />
-                        )}
-                    </Button>
-                }
-                backButton={
-                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowRight />
-                        ) : (
-                            <KeyboardArrowLeft />
-                        )}
-                        Back
-                    </Button>
-                }
-            />
-        </Box>
+                        </Box>
+                    </Card>
+                </Grid>
+
+                <Grid item>
+                    <Card sx={{bgcolor: "background.default"}}>
+                        <Box sx={{pl: 2, flexGrow: 1, width: 200}}>
+                            <Typography>Your top five: </Typography>
+
+                            <Typography>Your top five</Typography>
+
+                            <Typography>Your top five</Typography>
+
+                            <Typography>Your top five</Typography>
+
+                            <Typography>Your top five</Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+            </Grid>
+
+            <Card sx={{bgcolor: "background.default"}}>
+                <Grid container>
+                    <Grid item>
+                        <GameCard publisher={"Snowbird Games"} title={"Eador: Genesis"} img={"https://cdn.akamai.steamstatic.com/steam/apps/235660/header.jpg?t=1563274911"}/>
+                    </Grid>
+
+                    <Grid item>
+                        <GameCard publisher={"Snowbird Games"} title={"Eador: Genesis"} img={"https://cdn.akamai.steamstatic.com/steam/apps/236660/header.jpg?t=1447357742"}/>
+                    </Grid>
+
+                    <Grid item>
+                        <GameCard publisher={"Snowbird Games"} title={"Eador: Genesis"} img={"https://cdn.akamai.steamstatic.com/steam/apps/236660/header.jpg?t=1447357742"}/>
+                    </Grid>
+
+                    <Grid item>
+                        <GameCard publisher={"Snowbird Games"} title={"Eador: Genesis"} img={"https://cdn.akamai.steamstatic.com/steam/apps/236660/header.jpg?t=1447357742"}/>
+                    </Grid>
+
+                    <Grid item>
+                        <GameCard publisher={"Snowbird Games"} title={"Eador: Genesis"} img={"https://cdn.akamai.steamstatic.com/steam/apps/236660/header.jpg?t=1447357742"}/>
+                    </Grid>
+                </Grid>
+            </Card>
+
+
+        </>
     );
 }
--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,10 +1,92 @@
-import { Helmet } from 'react-helmet-async';
-import React, {useState, useEffect} from "react";
-// @mui
-import {alpha, useTheme} from '@mui/material/styles';
-import {Alert} from "@mui/lab";
+import React, { useState } from 'react';
+import {Box, Typography, TextField, Button, List, ListItem, ListItemText, Divider, Card, Avatar} from '@mui/material';
+import Iconify from "../components/iconify";
 
 export default function Messages() {
+    const [conversations, setConversations] = useState([
+        { id: 1, name: 'emruxx' },
+        { id: 2, name: 'keremmican' },
+        { id: 3, name: 'erennatala' },
+    ]);
 
-    return(<div>s</div>)
-}
+    const [messages, setMessages] = useState([
+        { id: 1, user: 'Emrullah', content: 'Selam' },
+        { id: 2, user: 'Kerem', content: 'Naber' },
+        { id: 3, user: 'Eren', content: 'Merhaba' },
+    ]);
+
+    const [newMessage, setNewMessage] = useState('');
+
+    const handleSendMessage = () => {
+        if (newMessage.trim() !== '') {
+            setMessages([...messages, { id: Date.now(), user: 'You', content: newMessage }]);
+            setNewMessage('');
+        }
+    };
+
+    const handleEnterKey = (event) => {
+        if (event.key === 'Enter') {
+            handleSendMessage();
+        }
+    };
+
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'row', height: '100%', p: 2 }}>
+            <Box sx={{ flexGrow: 0, width: '25%', borderRight: '1px solid #ccc', mr: 2 }}>
+                <Typography variant="h6" component="div" gutterBottom>
+                    Conversations
+                </Typography>
+                <List>
+                    {conversations.map((conversation) => (
+                        <React.Fragment key={conversation.id}>
+                            <Card>
+                                <ListItem button>
+                                    <Avatar src="" alt="photoURL" />
+                                    <Box sx={{flexGrow: 0.03}} />
+                                    <ListItemText primary={conversation.name} />
+                                </ListItem>
+                            </Card>
+                            <Divider />
+                        </React.Fragment>
+                    ))}
+                </List>
+            </Box>
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+                <Typography variant="h4" component="div" gutterBottom>
+                    Messages
+                </Typography>
+                <Box sx={{ flexGrow: 1, overflowY: 'auto', mb: 2 }}>
+                    <List>
+                        {messages.map((message) => (
+                            <Card key={message.id}>
+                                <ListItem key={message.id}>
+                                    <Avatar src="" alt="photoURL"/>
+                                    <Box sx={{flexGrow: 0.01}} />
+                                    <ListItemText primary={`${message.user}: ${message.content}`} />
+                                </ListItem>
+                            </Card>
+                        ))}
+                    </List>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                    <TextField
+                        fullWidth
+                        label="Type your message"
+                        variant="outlined"
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                        onKeyDown={handleEnterKey}
+                    />
+                    <Button variant="contained" onClick={handleSendMessage} sx={{ ml: 1 }}>
+                        Send
+                    </Button>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 1 }}>
+                    <Button variant="outlined" color="primary" startIcon={<Iconify icon={"flat-color-icons:video-call"} />}>
+                        Video Call
+                    </Button>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
