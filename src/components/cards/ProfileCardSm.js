1ec2c2e86dc358bb97eef1eb26f7093cf7678771
Kerem Mican <47419589+keremmican@users.noreply.github.com>
2023-05-27 04:26:45 +0300

    [Kerem Mican] fixxxx

--- src/components/cards/ProfileCardSm.js
+++ src/components/cards/ProfileCardSm.js
@@ -1,5 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import {Grid, Box, Avatar, IconButton, Link, ButtonBase, Typography, Dialog} from '@mui/material';
+import {
+    Grid,
+    Box,
+    Avatar,
+    IconButton,
+    Link,
+    ButtonBase,
+    Typography,
+    Dialog,
+    DialogTitle,
+    DialogActions, Button
+} from '@mui/material';
 import { styled } from "@mui/material/styles";
 import AddIcon from "@mui/icons-material/Add";
 import DoneIcon from "@mui/icons-material/Done";
@@ -24,6 +35,21 @@ export default function ProfileCardSm({ username, profilePhotoUrl, isFriend, onS
     const [isSent, setIsSent] = useState(false)
     const [isSender, setIsSender] = useState(false)
     const [showConfirmationDialog, setShowConfirmationDialog] = useState(false);
+    const [showNothing, setShowNothing] = useState(false)
+    const [areFriends, setAreFriends] = useState(isFriend)
+    const [isHovered, setIsHovered] = useState(false);
+    const [disable, setDisable] = useState(true)
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const username1 = await window.electron.ipcRenderer.invoke('getUsername');
+            if (username === username1) {
+                setShowNothing(true)
+            }
+        }
+
+        checkUser()
+    }, [])
 
     useEffect(() => {
         const checkFriendRequest = async () => {
@@ -32,7 +58,7 @@ export default function ProfileCardSm({ username, profilePhotoUrl, isFriend, onS
                 const loggedInUserId = await window.electron.ipcRenderer.invoke('getId');
 
                 const response = await axios.post(
-                    `${BASE_URL}users/checkFriendRequestByUsername?userId=${userId}&username=${username}`,
+                    `${BASE_URL}users/checkFriendRequestByUsername?userId=${loggedInUserId}&username=${username}`,
                     {},
                     {
                         headers: {
@@ -46,20 +72,26 @@ export default function ProfileCardSm({ username, profilePhotoUrl, isFriend, onS
                     setFriendRequestId(id);
 
                     if (loggedInUserId === senderId) {
-                        setIsSent(true)
                         setIsSender(true);
+                        setRequestSent(true)
+                        setDisable(false)
                     } else {
                         setIsSent(true)
                         setIsSender(false);
+                        setRequestSent(true)
                     }
                 }
             } catch (error) {
                 console.error('Error checking friend request:', error);
+            } finally {
+                setDisable(false)
             }
         };
 
-        checkFriendRequest()
-    }, [isFriend === false])
+        if (isFriend === false) {
+            checkFriendRequest()
+        }
+    }, [])
 
     const handleAdd = async () => {
         try {
@@ -75,8 +107,9 @@ export default function ProfileCardSm({ username, profilePhotoUrl, isFriend, onS
                     'Authorization': token
                 }
             });
+            setIsSender(true)
             setRequestSent(true)
-            onSuccess();
+            onSuccess("add");
         } catch (error) {
             console.log(error);
         }
@@ -84,15 +117,43 @@ export default function ProfileCardSm({ username, profilePhotoUrl, isFriend, onS
 
     const handleRemove = async () => {
         try {
+            const token = await window.electron.ipcRenderer.invoke('getToken');
+            const userId = await window.electron.ipcRenderer.invoke('getId');
 
+            const response = await axios.delete(`${BASE_URL}users/${userId}/removeByUsername/${username}`, {
+                headers: {
+                    Authorization: token
+                }
+            });
+
+            if (response.status === 200) {
+                setAreFriends(false)
+                onSuccess("remove");
+            } else {
+                console.log('Failed to remove friend');
+            }
         } catch (error) {
             console.log(error);
         }
-    }
+
+        setShowConfirmationDialog(false);
+    };
 
     const handleSure = async () => {
+        setShowConfirmationDialog(true);
+    };
 
-    }
+    const handleConfirmationDialogClose = () => {
+        setShowConfirmationDialog(false);
+    };
+
+    const handleConfirmationDialogYes = () => {
+        handleRemove();
+    };
+
+    const handleConfirmationDialogNo = () => {
+        setShowConfirmationDialog(false);
+    };
 
     const handleNavigate = async () => {
         const loggedinusername = await window.electron.ipcRenderer.invoke('getUsername');
@@ -103,11 +164,88 @@ export default function ProfileCardSm({ username, profilePhotoUrl, isFriend, onS
         }
     }
 
+    const removeRequest = async () => {
+        try {
+            const token = await window.electron.ipcRenderer.invoke('getToken');
+            const userId = await window.electron.ipcRenderer.invoke('getId');
+
+            const response = await axios.delete(`${BASE_URL}users/${userId}/removeRequest/${username}`, {
+                headers: {
+                    Authorization: token,
+                },
+            });
+
+            if (response.status === 200) {
+                setRequestSent(false);
+                setIsSent(false);
+                setIsSender(false);
+            } else {
+                console.log('Failed to remove friend request');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    const handleFriendButtonHover = () => {
+        setIsHovered(true);
+    };
+
+    const handleFriendButtonLeave = () => {
+        setIsHovered(false);
+    };
+
+    const handleAcceptRequest = async () => {
+        try {
+            const loggedInUserId = await window.electron.ipcRenderer.invoke('getId');
+            const token = await window.electron.ipcRenderer.invoke("getToken");
+            const response = await axios.post(`${BASE_URL}users/acceptFriendRequest/${friendRequestId}`,{},{
+                headers: {
+                    Authorization: token,
+                },
+            });
+
+            if (response.status === 200) {
+                setAreFriends(true);
+            } else {
+                console.error('Failed to accept friend request.');
+            }
+        } catch (error) {
+            console.error('Error accepting friend request:', error);
+        }
+    };
+
+    const handleDeclineRequest = async () => {
+        try {
+            const loggedInUserId = await window.electron.ipcRenderer.invoke('getId');
+            const token = await window.electron.ipcRenderer.invoke("getToken");
+
+            const response = await axios.post(`${BASE_URL}users/declineFriendRequest/${friendRequestId}`,{}, {
+                headers: {
+                    Authorization: token,
+                },
+            });
+
+            if (response.status === 200) {
+                setIsSent(false);
+            } else {
+                console.error('Failed to decline friend request.');
+            }
+        } catch (error) {
+            console.error('Error declining friend request:', error);
+        }
+    };
+
     return(
         <StyledAccount>
 
-            <Dialog open={showConfirmationDialog}>
-
+            <Dialog open={showConfirmationDialog} onClose={handleConfirmationDialogClose}>
+                <DialogTitle>Are you sure you want to remove {username}?</DialogTitle>
+                <DialogActions>
+                    <Button onClick={handleConfirmationDialogNo}>No</Button>
+                    <Button onClick={handleConfirmationDialogYes}>Yes</Button>
+                </DialogActions>
             </Dialog>
 
             <Grid container spacing={2}>
@@ -125,27 +263,41 @@ export default function ProfileCardSm({ username, profilePhotoUrl, isFriend, onS
                         </ButtonBase>
                     </Box>
                 </Grid>
-                {!isFriend ? (
-                    <Grid item xs={3}>
-                        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                            {!requestSent ? (<IconButton size="small" onClick={handleAdd}>
-                                <AddIcon />
-                            </IconButton>) : (
-                                <IconButton size="small" onClick={handleRemove}>
-                                    <DoneIcon />
+                {!showNothing &&
+                    (!areFriends ? (
+                        <Grid item xs={3}>
+                            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                {!requestSent ? (<IconButton size="small" onClick={handleAdd} disabled={disable}>
+                                    <AddIcon />
+                                </IconButton>) : (
+                                    isSender ? (<IconButton size="small" onClick={handleRemove}>
+                                        <Button
+                                            variant="contained"
+                                            size="small"
+                                            color={isHovered ? "error" : "warning"}
+                                            onClick={removeRequest}
+                                            onMouseEnter={handleFriendButtonHover}
+                                            onMouseLeave={handleFriendButtonLeave}
+                                        >
+                                            {isHovered ? "Cancel" : "Pending"}
+                                        </Button>
+                                    </IconButton>) : (
+                                       <>
+                                           <Button size="small" variant="contained" color="success" onClick={handleAcceptRequest}>Accept</Button>
+                                           <Button size="small" variant="contained" color="error" onClick={handleDeclineRequest}>Decline</Button>
+                                       </>
+                                ))}
+                            </Box>
+                        </Grid>
+                    ) : (
+                        <Grid item xs={3}>
+                            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                <IconButton size="small" onClick={handleSure}>
+                                    <RemoveCircleOutlineIcon />
                                 </IconButton>
-                            )}
-                        </Box>
-                    </Grid>
-                ) : (
-                    <Grid item xs={3}>
-                        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                            <IconButton size="small" onClick={handleSure}>
-                                <RemoveCircleOutlineIcon />
-                            </IconButton>
-                        </Box>
-                    </Grid>
-                )}
+                            </Box>
+                        </Grid>
+                    ))}
             </Grid>
         </StyledAccount>
     )
